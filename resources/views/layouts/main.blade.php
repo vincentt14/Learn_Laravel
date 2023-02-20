0ec74c7eee49fde909a1a6a8ba7b8f528ebd3d30
Vincent <vincent14@student.umn.ac.id>
2023-02-20 17:25:16 +0700

    learning blade templating & routes

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.rtl.min.css" integrity="sha384-WJUUqfoMmnfkBLne5uxXj+na/c7sesSJ32gI7GfCk4zO4GthUKhSEGyvQ839BC51" crossorigin="anonymous">
+
+    <title>{{ $title }}</title>
+  </head>
+  <body>
+    @include('partials.navbar')
+
+    <div class="container mt-4">
+      @yield('container')
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+  </body>
+</html>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Blog | Post</title>
-</head>
-<body>
-  <h1>Halaman Post</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+  <article>
+    <h2>{{ $post['title'] }}</h2>
+    <h5>{{ $post['author'] }}</h5>
+    <p>{{ $post['body'] }}</p>
+  </article>
+
+  <a href="/blog">Back to Posts</a>
+@endsection
