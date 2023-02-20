756cc0e8cae3efb61cd6a59dcb07fffa6d05e6b0
Vincent <vincent14@student.umn.ac.id>
2023-02-20 15:17:51 +0700

    laravel new app

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/laravel', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Vincent",
+        "email" => "vincent@gmail.com",
+        "image" => "dicoding.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('post');
+});
