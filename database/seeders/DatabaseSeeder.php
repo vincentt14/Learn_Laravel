808d74d374dd551a52551fedb77b1e34e7f26178
Vincent <vincent14@student.umn.ac.id>
2023-02-27 17:33:13 +0700

    adding post factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,24 +15,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        User::create([
-            'name' => 'Vincent',
-            'email' => 'vincent@gmail.com',
-            'password' => bcrypt('vincent123')
-        ]);
+        // User::create([
+        //     'name' => 'Vincent',
+        //     'email' => 'vincent@gmail.com',
+        //     'password' => bcrypt('vincent123')
+        // ]);
 
-        User::create([
-            'name' => 'Doddy',
-            'email' => 'doddy@gmail.com',
-            'password' => bcrypt('dodidoditot')
-        ]);
+        // User::create([
+        //     'name' => 'Doddy',
+        //     'email' => 'doddy@gmail.com',
+        //     'password' => bcrypt('dodidoditot')
+        // ]);
+
+        User::factory(5)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -44,31 +46,33 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            "title" => 'Judul Pertama',
-            "slug" => 'judul-pertama',
-            "excerp" => 'nasi padang',
-            'body' => 'nasi padang enak sekale',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            "title" => 'Type Script',
-            "slug" => 'type-script',
-            "excerp" => 'TS VS JS',
-            'body' => '<p>TS VS JS</p><p>Hayoo menang siapa hayuuk</p>',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     "title" => 'Judul Pertama',
+        //     "slug" => 'judul-pertama',
+        //     "excerp" => 'nasi padang',
+        //     'body' => 'nasi padang enak sekale',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
 
-        Post::create([
-            "title" => 'Babi Panggang',
-            "slug" => 'babi-panggang',
-            "excerp" => 'Babi panggang atau goreng lebi enak',
-            'body' => '<p>babi panggang atau goreng lebi enak</p><p>Hayoo menang siapa hayuuk</p>',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     "title" => 'Type Script',
+        //     "slug" => 'type-script',
+        //     "excerp" => 'TS VS JS',
+        //     'body' => '<p>TS VS JS</p><p>Hayoo menang siapa hayuuk</p>',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     "title" => 'Babi Panggang',
+        //     "slug" => 'babi-panggang',
+        //     "excerp" => 'Babi panggang atau goreng lebi enak',
+        //     'body' => '<p>babi panggang atau goreng lebi enak</p><p>Hayoo menang siapa hayuuk</p>',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
     }
 }
