9729bd1f59ef5ee4449e2d0d829aa3020ce8307c
Vincent <vincent14@student.umn.ac.id>
2023-02-28 16:10:33 +0700

    fixing categories ui

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,7 @@ class PostController extends Controller
     {
         return view('posts', [
             "title" => "All Posts",
+            "active" => 'posts',
             // "posts" => Post::all()
             "posts" => Post::latest()->get()
         ]);
@@ -19,6 +20,7 @@ class PostController extends Controller
     public function show(Post $post){
         return view('post', [
             "title" => "Single Post",
+            "active" => 'posts',
             "post" => $post
         ]);
     }
--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,16 +1,24 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-5">Categories</h1>
-@foreach ($categories as $category)
-<ul>  
-  <li class="mb-3">
-    <h2>
-      <a href="/categories/{{ $category->slug }}" class="text-decoration-none">{{ $category->name }}</a>
-    </h2>
-  </li>
-</ul>
-@endforeach
+  <h1 class="mb-5">Posts Categories</h1>
 
+  <div class="container">
+    <div class="row">
+      @foreach ($categories as $category)
+        <div class="col-md-4">
+          <a href="/categories/{{ $category->slug }}">
+            <div class="card text-bg-dark">
+              <img src="https://source.unsplash.com/500x500?{{ $category->name }}" class="card-img"
+                alt="{{ $category->name }}">
+              <div class="card-img-overlay d-flex align-items-center p-0">
+                <h5 class="card-title flex-fill fs-3 p-4 text-center" style="background-color: rgba(0,0,0,0.7)">
+                  {{ $category->name }}</h5>
+              </div>
+            </div>
+          </a>
+        </div>
+      @endforeach
+    </div>
+  </div>
 @endsection
-
