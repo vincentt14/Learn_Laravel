e77ad7d04777a43345e18ce42f27e4106630ca8b
Vincent <vincent14@student.umn.ac.id>
2023-03-01 16:39:29 +0700

    adding form login and validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,4 +13,16 @@ class RegisterController extends Controller
             'active' => 'register'
         ]);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        dd($request);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,7 @@ Route::get('/categories', function () {
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
 
 
 // SUDAH DITANGANI OLEH QUERY DI MODEL POST.PHP
