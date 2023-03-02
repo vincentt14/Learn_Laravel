32dddc7eb46c75d3c6d6b095bb1d81d7931da858
Vincent <vincent14@student.umn.ac.id>
2023-03-02 13:31:56 +0700

    autentikasi login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -17,11 +17,17 @@ class LoginController extends Controller
 
     public function authenticate(Request $request)
     {
-        $request->validate([
+        $credentials = $request->validate([
             'email' => 'required|email:dns',
             'password' => 'required'
         ]);
 
-        dd('berhasil login');
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->with('loginError', 'Login failed!');
     }
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -11,6 +11,13 @@
         </div>
       @endif
 
+      @if (session()->has('loginError'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          {{ session('loginError') }}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      @endif
+
       <main class="form-signin w-100 m-auto">
         <h1 class="h3 fw-normal mb-3 text-center">Please login</h1>
         <form action="/login" method="post">
