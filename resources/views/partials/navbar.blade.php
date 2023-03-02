8e93b686c37e30f8d25fd6ec9a90153e78dc1803
Vincent <vincent14@student.umn.ac.id>
2023-03-02 13:51:29 +0700

    adding middleware auth

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,29 +1,50 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container">
     <a class="navbar-brand" href="/">Blog</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="navbar-collapse collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link {{ ($active == "home") ? "active" : "" }}" href="/">Home</a>
+          <a class="nav-link {{ $active == 'home' ? 'active' : '' }}" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($active == "about") ? "active" : "" }}" href="/about">About</a>
+          <a class="nav-link {{ $active == 'about' ? 'active' : '' }}" href="/about">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($active == "posts") ? "active" : "" }}" href="/posts">Blog</a>
+          <a class="nav-link {{ $active == 'posts' ? 'active' : '' }}" href="/posts">Blog</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($active == "categories") ? "active" : "" }}" href="/categories">Categories</a>
+          <a class="nav-link {{ $active == 'categories' ? 'active' : '' }}" href="/categories">Categories</a>
         </li>
       </ul>
+
       <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link {{ ($active == "login") ? "active" : "" }}" href="/login"><i class="bi bi-box-arrow-in-right"></i>Login</a>
-        </li>
+        @auth
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Welcome Back, {{ auth()->user()->name }}
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My
+                  Dashboard</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i> Logout</a></li>
+            </ul>
+          </li>
+        @else
+          <li class="nav-item">
+            <a class="nav-link {{ $active == 'login' ? 'active' : '' }}" href="/login"><i
+                class="bi bi-box-arrow-in-right"></i> Login</a>
+          </li>
+        @endauth
       </ul>
+
     </div>
   </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -50,12 +51,13 @@ Route::get('/categories', function () {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
 Route::get('/register', [RegisterController::class, 'index']);
 
 Route::post('/register', [RegisterController::class, 'store']);
 Route::post('/login', [LoginController::class, 'authenticate']);
 
+Route::get('/dashboard', [DashboardController::class, 'index']);
 
 // SUDAH DITANGANI OLEH QUERY DI MODEL POST.PHP
 
