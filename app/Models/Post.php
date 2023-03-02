7d5a6f7e311ab8ace153986bd8e325ce7ad23073
Vincent <vincent14@student.umn.ac.id>
2023-03-02 17:16:24 +0700

    adding detail for admins

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,21 +13,23 @@ class Post extends Model
     protected $with = ['author', 'category'];
 
     public function scopeFilter($query, array $filters)
-    {   
-        $query->when($filters['search'] ?? false, function($query, $search){
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
             return $query->where('title', 'like', '%' . $search . '%')->orWhere('body', 'like', '%' . $search . '%');
         });
 
-        $query->when($filters['category'] ?? false, function($query, $category){
-            return $query->whereHas('category', function($query) use($category){
+        $query->when($filters['category'] ?? false, function ($query, $category) {
+            return $query->whereHas('category', function ($query) use ($category) {
                 $query->where('slug', $category);
-            });
+            }
+            );
         });
 
-        $query->when($filters['author'] ?? false, function($query, $author){
-            return $query->whereHas('author', function($query) use($author){
+        $query->when($filters['author'] ?? false, function ($query, $author) {
+            return $query->whereHas('author', function ($query) use ($author) {
                 $query->where('username', $author);
-            });
+            }
+            );
         });
     }
 
@@ -39,5 +41,10 @@ class Post extends Model
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -4,4 +4,29 @@
   <div class="d-flex justify-content-between flex-md-nowrap align-items-center border-bottom mb-3 flex-wrap pt-3 pb-2">
     <h1 class="h2">My posts</h1>
   </div>
+
+  <div class="table-responsive col-lg-8">
+    <table class="table-striped table-sm table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Title</th>
+          <th scope="col">Category</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($posts as $post)
+          <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->category->name }}</td>
+            <td><a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info">view</a></td>
+            <td><a href="/" class="badge bg-warning">edit</a></td>
+            <td><a href="/" class="badge bg-danger">delete</a></td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 @endsection
