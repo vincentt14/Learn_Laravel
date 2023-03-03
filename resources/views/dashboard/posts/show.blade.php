a17b9c9bcbc7b0f19ed9ee2693dd30a55e5d9a69
Vincent <vincent14@student.umn.ac.id>
2023-03-03 17:23:36 +0700

    adding uploaded image to view

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -12,8 +12,14 @@
           @csrf
           <button class="btn btn-danger border-0" onClick="return confirm('Are you sure?')">Delete</button>
         </form>
-        <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid mt-3"
-          alt="{{ $post->category->name }}">
+        @if ($post->image)
+          <div style="max-height: 350px; overflow:hidden">
+            <img src="{{ asset('storage/' . $post->image) }}" class="img-fluid mt-3" alt="{{ $post->category->name }}">
+          </div>
+        @else
+          <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid mt-3"
+            alt="{{ $post->category->name }}">
+        @endif
         <article class="my-3">
           {!! $post->body !!}
         </article>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,10 +6,15 @@
       <div class="col-md-8">
         <h2 class="mb-3">{{ $post->title }}</h2>
         <p class="mb-3">By: <a href="/posts?author={{ $post->author->username }}"
-            class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}"
-            class="text-decoration-none">{{ $post->category->name }}</a></p>
-        <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid"
-          alt="{{ $post->category->name }}">
+            class="text-decoration-none">{{ $post->author->name }}</a> in <a
+            href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
+        </p>
+        @if ($post->image)
+          <img src="{{ asset('storage/' . $post->image) }}" class="img-fluid" alt="{{ $post->category->name }}">
+        @else
+          <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid"
+            alt="{{ $post->category->name }}">
+        @endif
         <article class="my-3">
           {!! $post->body !!}
         </article>
