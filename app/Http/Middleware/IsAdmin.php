209f6ff007d33092a4ed9c21dc20356ffd3e00a6
Vincent <vincent14@student.umn.ac.id>
2023-03-06 15:45:40 +0700

    adding gate and new admin to user table

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -15,7 +15,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(!auth()->check() || auth()->user()->username !== 'vincent'){
+        if(!auth()->check() || !auth()->user()->is_admin){
             abort(403);
         }
         return $next($request);
--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -15,18 +15,20 @@
       </li>
     </ul>
 
-    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-      <span>Administrator</span>
-    </h6>
-    <ul class="nav flex-column">
-      <li class="nav-item">
-        <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" aria-current="page" href="/dashboard/categories">
-          <span data-feather="home" class="align-text-bottom"></span>
-          Posts Categories
-        </a>
-      </li>
-    </ul>
-
+    @can('admin')
+      <h6 class="sidebar-heading d-flex justify-content-between align-items-center text-muted mt-4 mb-1 px-3">
+        <span>Administrator</span>
+      </h6>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" aria-current="page"
+            href="/dashboard/categories">
+            <span data-feather="home" class="align-text-bottom"></span>
+            Posts Categories
+          </a>
+        </li>
+      </ul>
+    @endcan
     </ul>
   </div>
 </nav>
